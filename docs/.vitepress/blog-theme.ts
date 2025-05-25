b5b7f1a5cc0d4ef7efd0799857c0a0d596696533
相甫 <xiangfu_git@163.com>
2025-05-25 23:43:50 +0800

    修改

--- docs/.vitepress/blog-theme.ts
+++ docs/.vitepress/blog-theme.ts
@@ -31,7 +31,7 @@ const blogTheme = getThemeConfig({
   footer: {
     // message 字段支持配置为HTML内容，配置多条可以配置为数组
     // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
-    copyright: `相甫 2025 - ${new Date().getFullYear()}`,
+    copyright: `相甫 2016 - ${new Date().getFullYear()}`,
     // icpRecord: {
     //   name: '蜀ICP备19011724号',
     //   link: 'https://beian.miit.gov.cn/'
